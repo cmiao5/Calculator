2644c6ca56481696b101950be282701403315240
cmiao5 <cmiao5@illinois.edu>
2019-04-18 19:37:01 -0500

    final calc

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -2,92 +2,153 @@ package edu.illinois.cs.cs125.spring2019.lab12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Main class for our UI design lab.
  */
-public final class MainActivity extends AppCompatActivity {
-    /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "Lab12:Main";
-
-    /** Request queue for our API requests. */
-    private static RequestQueue requestQueue;
-
+public class MainActivity extends AppCompatActivity {
     /**
-     * Run when this activity comes to the foreground.
-     *
-     * @param savedInstanceState unused
+     * .
+     */
+    private TextView result;
+    /**
+     * .
+     */
+    private EditText number1, number2;
+    /**
+     * .
+     */
+    private Button add, subtract, divide, multiply;
+    /**
+     * .
+     */
+    private float resultNum;
+    /**
+     * .
+     */
+    private int num1, num2;
+    /**
+     * .
+     * @param savedInstanceState = .
      */
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set up the queue for our API requests
-        requestQueue = Volley.newRequestQueue(this);
-
         setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        number1 = (EditText) findViewById(R.id.number1);
+        number2 = (EditText) findViewById(R.id.number2);
+        add = (Button) findViewById(R.id.add);
+        subtract = (Button) findViewById(R.id.minus);
+        multiply = (Button) findViewById(R.id.multiply);
+        divide = (Button) findViewById(R.id.divide);
 
-        startAPICall("192.17.96.8");
+        /**
+         * .
+         */
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * .
+             */
+            public void onClick(final View v) {
+
+                /**
+                 * .
+                 */
+                num1 = Integer.parseInt(number1.getText().toString());
+                num2 = Integer.parseInt(number2.getText().toString());
+                resultNum = num1 + num2;
+                result.setText(String.valueOf(resultNum));
+
+            }
+        });
+
+        /**
+         * .
+         */
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * .
+             */
+            public void onClick(final View v) {
+
+                /**
+                 * .
+                 */
+                num1 = Integer.parseInt(number1.getText().toString());
+                num2 = Integer.parseInt(number2.getText().toString());
+                resultNum = num1 + num2;
+                result.setText(String.valueOf(resultNum));
+
+            }
+        });
+        /**
+         * .
+         */
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * .
+             */
+            public void onClick(final View v) {
+
+                /**
+                 * .
+                 */
+                num1 = Integer.parseInt(number1.getText().toString());
+                num2 = Integer.parseInt(number2.getText().toString());
+                resultNum = num1 - num2;
+                result.setText(String.valueOf(resultNum));
+
+            }
+        });
+        /**
+         * .
+         */
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * .
+             */
+            public void onClick(final View v) {
+
+                /**
+                 * .
+                 */
+                num1 = Integer.parseInt(number1.getText().toString());
+                num2 = Integer.parseInt(number2.getText().toString());
+                resultNum = num1 / num2;
+                result.setText(String.valueOf(resultNum));
+
+            }
+        });
+        /**
+         * .
+         */
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            /**
+             * .
+             */
+            public void onClick(final View v) {
+
+                /**
+                 * .
+                 */
+                num1 = Integer.parseInt(number1.getText().toString());
+                num2 = Integer.parseInt(number2.getText().toString());
+                resultNum = num1 * num2;
+                result.setText(String.valueOf(resultNum));
+
+            }
+        });
+
     }
 
-    /**
-     * Run when this activity is no longer visible.
-     */
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    /**
-     * Make a call to the IP geolocation API.
-     *
-     * @param ipAddress IP address to look up
-     */
-    void startAPICall(final String ipAddress) {
-        try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "https://ipinfo.io/" + ipAddress + "/json",
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            apiCallDone(response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(final VolleyError error) {
-                            Log.e(TAG, error.toString());
-                        }
-                    });
-            jsonObjectRequest.setShouldCache(false);
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Handle the response from our IP geolocation API.
-     *
-     * @param response response from our IP geolocation API.
-     */
-    void apiCallDone(final JSONObject response) {
-        try {
-            Log.d(TAG, response.toString(2));
-            // Example of how to pull a field off the returned JSON object
-            Log.i(TAG, response.get("hostname").toString());
-        } catch (JSONException ignored) { }
-    }
 }
