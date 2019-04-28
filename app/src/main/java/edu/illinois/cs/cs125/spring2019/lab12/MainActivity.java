171f3f415c9fc1c1847c328294eadec1e318278f
cmiao5 <cmiao5@illinois.edu>
2019-04-28 15:45:41 -0500

    calculator done

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -59,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        acelVal = SensorManager.GRAVITY_EARTH;
+        acelLast = SensorManager.GRAVITY_EARTH;
+        shake = 0.00f;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         result = (TextView) findViewById(R.id.result);
@@ -205,40 +213,22 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * .
-     * @param savedInstanceState .
-     */
-    protected void onClick(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-
-        acelVal = SensorManager.GRAVITY_EARTH;
-        acelLast = SensorManager.GRAVITY_EARTH;
-        shake = 0.00f;
-
-    }
-
-    /**
-     * .
      */
     private final SensorEventListener sensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(final SensorEvent sensorEvent) {
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
 
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            acelLast = acelVal;
-            acelVal = (float) Math.sqrt((double) (x * x + y * y + z * z));
-            float delta = acelVal - acelLast;
-            shake = shake * 1.0f + delta;
-            if (shake > 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1) {
-                resultNum = resultNum * resultNum;
-                result.setText(String.valueOf(resultNum));
-            }
+                acelLast = acelVal;
+                acelVal = (float) Math.sqrt((double) (x * x + y * y + z * z));
+                float delta = acelVal - acelLast;
+                shake = shake * 0.9f + delta;
+                if (shake > 12) {
+                    resultNum = resultNum * resultNum;
+                    result.setText(String.valueOf(resultNum));
+                }
 
 
 
